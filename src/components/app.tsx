ac755407497db32d2aac7a18f8b82d2509df3a4e
David First <david@cocycles.com>
2017-11-06 14:17:07 -0500

    add bit.json

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,11 +3,15 @@ import {properties, stylable} from 'wix-react-tools';
 
 import {Header} from './header';
 import {Content} from './content';
+import {List} from './demo/list/src/components/list';
 import style from './app.st.css';
 
 export const App: React.SFC = stylable(style)(properties(() => (
     <div>
-        <Header message="Welcome to React with Stylable!!!" />
+        <Header message="Welcome to React with Stylable" />
+        <div>
+            <List />
+        </div>
         <Content mainFile="src/index.tsx" />
     </div>
 )));
